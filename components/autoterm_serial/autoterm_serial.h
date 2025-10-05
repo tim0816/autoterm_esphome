aa2b7331ca0d5fc5e36e0edbc0bd81650a5d4cdd
Tim <105344588+tim0816@users.noreply.github.com>
2025-10-05 21:38:56 +0200

    Update autoterm_serial.h

--- components/autoterm_serial/autoterm_serial.h
+++ components/autoterm_serial/autoterm_serial.h
@@ -4,13 +4,14 @@ class AutotermHeaterComponent : public PollingComponent {
  public:
   UARTDevice *uart;
 
+  // Sensoren
   Sensor *temperature_sensor = new Sensor();
   Sensor *voltage_sensor = new Sensor();
   Sensor *status_sensor = new Sensor();
 
   std::vector<uint8_t> buffer;
 
-  AutotermHeaterComponent(UARTComponent *parent) : PollingComponent(1000), uart(parent) {}
+  AutotermHeaterComponent(UARTComponent *uart_) : PollingComponent(1000), uart(uart_) {}
 
   void setup() override {}
   void update() override;
